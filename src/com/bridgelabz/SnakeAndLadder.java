2515f5f76121809baf52b51d204dddf44015adac
Arun <rarnkmrbridgelabz.com>
2022-03-21 23:33:11 +0530

    Added UC-2

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
         System.out.println("Welcome to Snake & Ladder Game");
         String player1;
         int startposition = 0;
+        int dicenumber = (int) (Math.random()*6)+1;
+        System.out.println("Your Random Dice Number is:"+dice);
     }
 }

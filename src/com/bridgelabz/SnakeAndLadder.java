7d0de518ec65f8e007b0dbe7a5ca5e526ce0296e
Arun <rarnkmrbridgelabz.com>
2022-03-22 01:02:44 +0530

    Added UC-7

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,34 +4,53 @@ public class SnakeAndLadder {
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake & Ladder Game");
-        String player1;
-        int startposition = 0;
-        int rolls = 0;
-        while(startposition<100) {
-            int dicenumber = (int) (Math.random() * 6) + 1;
-            rolls++;
-            System.out.println("Your Random Dice Number(1to6) is:" + dicenumber);
-            int option = (int) (Math.random() * 3);
-            System.out.println("Your Random options(0,1,2) is: " + option);
-            switch (option) {
-                case LADDER:
-                    startposition = startposition + dicenumber;
-                    break;
-                case SNAKE:
-                    startposition = startposition - dicenumber;
-                    break;
+
+    public static void main(String[] args){
+        int position1 = 0;
+        int position2 = 0;
+        int turn = 1;
+        while (position1 < 100 && position2<100) {
+            if (turn == 1) {
+                position1 = DiceRoll(position1);
+                turn = 2;
             }
-            System.out.println("startposition : " + startposition);
-            if(startposition<0){
-                startposition=0;
+            else {
+                position2 = DiceRoll(position2);
+                turn = 1;
             }
-            if(startposition>100){
-                startposition=startposition-dicenumber;
-            }
-            System.out.println("Position after every dice roll: "+startposition);
         }
-        System.out.println("Total number of times the dice played to win the Game: "+rolls);
+        System.out.println("Position1 :"+position1+" Position2 :"+position2);
+        if (position1 == 100) {
+            System.out.println("Player1 is the winner");
+        }
+        else {
+            System.out.println("Player2 is the winner");
+        }
+    }
+
+    public static int DiceRoll(int position) {
+        int dice = (int) (Math.random()*6)+1;
+        int option = (int) (Math.random()*3);
+
+        switch (option) {
+            case LADDER :
+                position = position + dice;
+                break;
+            case SNAKE :
+                position = position - dice;
+                break;
+            case NO_PLAY :
+                break;
+        }
+        if (position<0) {
+            position = 0;
+        }
+        if (position > 100) {
+            position = position - dice;
+        }
+        return position;
     }
 }
+
+
+
